ab341084cbb52aa473fa9fd8cb66173dc6103b7e
OmarDegani <omar.degani@esprit.tn>
2025-11-18 00:57:34 +0100

    Fix tests: add MockBean for repositories to prevent DataSource dependency in tests

--- src/test/java/com/abdessalem/finetudeingenieurworkflow/FinEtudeIngenieurWorkflowApplicationTests.java
+++ src/test/java/com/abdessalem/finetudeingenieurworkflow/FinEtudeIngenieurWorkflowApplicationTests.java
@@ -1,22 +1,33 @@
 package com.abdessalem.finetudeingenieurworkflow;
 
+import com.abdessalem.finetudeingenieurworkflow.Repository.IAdminRepository;
+import com.abdessalem.finetudeingenieurworkflow.Repository.IUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(exclude = {
     DataSourceAutoConfiguration.class,
     HibernateJpaAutoConfiguration.class,
     SqlInitializationAutoConfiguration.class
 }, properties = {
-    "spring.data.jpa.repositories.enabled=false"
+    "spring.data.jpa.repositories.enabled=false",
+    "spring.main.allow-bean-definition-overriding=true"
 })
 class FinEtudeIngenieurWorkflowApplicationTests {
 
+    @MockBean
+    private IUserRepository userRepository;
+
+    @MockBean
+    private IAdminRepository adminRepository;
+
     @Test
     void contextLoads() {
+        // Test passes if context loads successfully
     }
 
 }
