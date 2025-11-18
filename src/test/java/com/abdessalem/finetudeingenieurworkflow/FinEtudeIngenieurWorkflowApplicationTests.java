459c10f9689a2097462675cd8be4c4958c614c22
OmarDegani <omar.degani@esprit.tn>
2025-11-18 01:08:43 +0100

    Simplify test: remove all MockBeans and exclude security auto-configuration, use minimal test setup

--- src/test/java/com/abdessalem/finetudeingenieurworkflow/FinEtudeIngenieurWorkflowApplicationTests.java
+++ src/test/java/com/abdessalem/finetudeingenieurworkflow/FinEtudeIngenieurWorkflowApplicationTests.java
@@ -1,107 +1,23 @@
 package com.abdessalem.finetudeingenieurworkflow;
 
-import com.abdessalem.finetudeingenieurworkflow.Repository.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration,org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration",
-    "spring.data.jpa.repositories.enabled=false",
-    "spring.main.allow-bean-definition-overriding=true",
-    "spring.mail.username=test@example.com",
-    "spring.mail.host=smtp.example.com",
-    "spring.mail.port=587"
-})
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.NONE,
+    properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration,org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration,org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration",
+        "spring.data.jpa.repositories.enabled=false",
+        "spring.main.allow-bean-definition-overriding=true",
+        "spring.mail.username=test@example.com",
+        "spring.mail.host=smtp.example.com",
+        "spring.mail.port=587",
+        "spring.main.web-application-type=none"
+    }
+)
+@ActiveProfiles("test")
 class FinEtudeIngenieurWorkflowApplicationTests {
-
-    @MockBean
-    private IUserRepository userRepository;
-
-    @MockBean
-    private IAdminRepository adminRepository;
-
-    @MockBean
-    private ITuteurRepository tuteurRepository;
-
-    @MockBean
-    private ISujetRepository sujetRepository;
-
-    @MockBean
-    private ISocieteRepository societeRepository;
-
-    @MockBean
-    private IEtudiantRepository etudiantRepository;
-
-    @MockBean
-    private IEquipeRepository equipeRepository;
-
-    @MockBean
-    private IProjetRepository projetRepository;
-
-    @MockBean
-    private IFormRepository formRepository;
-
-    @MockBean
-    private IFormResponseRepository formResponseRepository;
-
-    @MockBean
-    private IFormFieldRepository formFieldRepository;
-
-    @MockBean
-    private ITacheRepository tacheRepository;
-
-    @MockBean
-    private IHistoriqueRepository historiqueRepository;
-
-    @MockBean
-    private IBacklogRepository backlogRepository;
-
-    @MockBean
-    private IEpicRepository epicRepository;
-
-    @MockBean
-    private ISprintRepository sprintRepository;
-
-    @MockBean
-    private ICandidatureRepository candidatureRepository;
-
-    @MockBean
-    private IEvaluationGridRepository evaluationGridRepository;
-
-    @MockBean
-    private IEvaluationCriterionRepository evaluationCriterionRepository;
-
-    @MockBean
-    private ICriterionLevelRepository criterionLevelRepository;
-
-    @MockBean
-    private IAppreciationRepository appreciationRepository;
-
-    @MockBean
-    private IJustificationRepository justificationRepository;
-
-    @MockBean
-    private IStudentEvaluationRepository studentEvaluationRepository;
-
-    @MockBean
-    private IStudentAnalysisReportRepository studentAnalysisReportRepository;
-
-    @MockBean
-    private ISuggestionRepository suggestionRepository;
-
-    @MockBean
-    private MotivationScoreRepository motivationScoreRepository;
-
-    @MockBean
-    private EtatHistoriqueTacheRepository etatHistoriqueTacheRepository;
-
-    @MockBean
-    private CodeAnalysisResultRepository codeAnalysisResultRepository;
-
-    @MockBean
-    private JavaMailSender javaMailSender;
 
     @Test
     void contextLoads() {
