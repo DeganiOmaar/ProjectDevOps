fbed4138ee3fc6e582457ecd3fc6569aafb8b9cd
OmarDegani <omar.degani@esprit.tn>
2025-11-18 01:01:52 +0100

    Clean up unused imports in test class

--- src/test/java/com/abdessalem/finetudeingenieurworkflow/FinEtudeIngenieurWorkflowApplicationTests.java
+++ src/test/java/com/abdessalem/finetudeingenieurworkflow/FinEtudeIngenieurWorkflowApplicationTests.java
@@ -4,10 +4,7 @@ import com.abdessalem.finetudeingenieurworkflow.Repository.IAdminRepository;
 import com.abdessalem.finetudeingenieurworkflow.Repository.IUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 @SpringBootTest(properties = {
     "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration",
